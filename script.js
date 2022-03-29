834a414f4cd0e9f6e142d149699dbb381857c774
Zovma <dim.borzov@gmail.com>
2022-03-29 10:53:24 +0300

    debag

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/script', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
@@ -266,4 +266,4 @@ function useMitingProductPresentationNoMatterWhere(dataScript) {
 
 
 
-app.listen(3000);
+app.listen(3001);
