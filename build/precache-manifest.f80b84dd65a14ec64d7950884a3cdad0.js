ed812272ead96c853a325c2f72657eac02fa7739
Zovma <dim.borzov@gmail.com>
2022-03-29 10:37:04 +0300

    debag

---
+++ build/precache-manifest.f80b84dd65a14ec64d7950884a3cdad0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e52975e3d486b6967652",
+    "url": "/static/css/main.c3ad2097.chunk.css"
+  },
+  {
+    "revision": "e52975e3d486b6967652",
+    "url": "/static/js/main.e52975e3.chunk.js"
+  },
+  {
+    "revision": "a3c0bc647fa7f281b58f",
+    "url": "/static/js/1.a3c0bc64.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0bb5bac2f1acaf01ce9fd6e95c5db456",
+    "url": "/index.html"
+  }
+];
